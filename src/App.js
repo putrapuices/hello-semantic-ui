041012e2921d70b3c86321282bb64a2f9cd10770
putrapuices <puices14@gmail.com>
2021-12-05 10:58:55 +0700

    Semantic UI - Container ( 88 )

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React, { Component } from "react";
 import "semantic-ui-css/semantic.min.css";
-import { Button, Grid } from "semantic-ui-react";
+import { Button, Container, Grid } from "semantic-ui-react";
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Container textAlign="left">
+          Kiri Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Doloribus eveniet libero ullam beatae, impedit modi optio nesciunt,
+          corporis doloremque dolores veritatis molestias obcaecati tempora
+          natus maxime nostrum voluptatem similique minima.*30
+        </Container>
+        <br />
+        <br />
+
+        <Container textAlign="right">
+          Kiri Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Doloribus eveniet libero ullam beatae, impedit modi optio nesciunt,
+          corporis doloremque dolores veritatis molestias obcaecati tempora
+          natus maxime nostrum voluptatem similique minima.*30
+        </Container>
+        <br />
+        <br />
+        <Container textAlign="center">
+          Kiri Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Doloribus eveniet libero ullam beatae, impedit modi optio nesciunt,
+          corporis doloremque dolores veritatis molestias obcaecati tempora
+          natus maxime nostrum voluptatem similique minima.*30
+        </Container>
+        <br />
+        <br />
+        <Container textAlign="justified">
+          Kiri Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Doloribus eveniet libero ullam beatae, impedit modi optio nesciunt,
+          corporis doloremque dolores veritatis molestias obcaecati tempora
+          natus maxime nostrum voluptatem similique minima.*30
+        </Container>
         <h1>Selamat Datnag React Sematic UI</h1>
 
         <Grid container columns={4}>
