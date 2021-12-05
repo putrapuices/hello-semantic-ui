71743ef0a17392bccda33604c1e1ba471250c59d
putrapuices <puices14@gmail.com>
2021-12-05 10:53:06 +0700

    Semantic UI - Button ( 87 )

--- src/App.js
+++ src/App.js
@@ -7,11 +7,15 @@ class App extends Component {
     return (
       <div>
         <h1>Selamat Datnag React Sematic UI</h1>
-        <Button primary>Button Pertama</Button>
-        <Button secondary>Button Kedua</Button>
 
         <Grid container columns={4}>
-          <Grid.Column>1</Grid.Column>
+          <Grid.Column>
+            <Button primary>Button Pertama</Button>
+            <Button secondary>Button Kedua</Button>
+            <Button basic color="red">
+              Button Ketiga
+            </Button>
+          </Grid.Column>
           <Grid.Column>2</Grid.Column>
           <Grid.Column>3</Grid.Column>
           <Grid.Column>4</Grid.Column>
