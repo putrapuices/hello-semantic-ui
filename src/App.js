a159474ab2e06da41129e56ec5f522afc482c3bd
putrapuices <puices14@gmail.com>
2021-12-05 10:49:57 +0700

    Semantic UI - Layout ( 86 )

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "semantic-ui-css/semantic.min.css";
-import { Button } from "semantic-ui-react";
+import { Button, Grid } from "semantic-ui-react";
 
 class App extends Component {
   render() {
@@ -9,6 +9,13 @@ class App extends Component {
         <h1>Selamat Datnag React Sematic UI</h1>
         <Button primary>Button Pertama</Button>
         <Button secondary>Button Kedua</Button>
+
+        <Grid container columns={4}>
+          <Grid.Column>1</Grid.Column>
+          <Grid.Column>2</Grid.Column>
+          <Grid.Column>3</Grid.Column>
+          <Grid.Column>4</Grid.Column>
+        </Grid>
       </div>
     );
   }
